8e299bf0c7df6de8955a0d8e4fabb481c8f8e0d2
David Li <dtli@lcs.on.ca>
2023-04-20 08:35:43 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by David Li on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
